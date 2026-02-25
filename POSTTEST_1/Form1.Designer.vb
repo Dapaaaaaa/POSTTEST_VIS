94aaeab43f5e1d8b4d11c0c10b6272296cf35126
Dapaaaaaa <daffaharahap191205@gmail.com>
2026-02-25 23:42:34 +0800

    feat: Kalkulasi IPK (POSTTEST 1)

--- POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/Form1.Designer.vb
@@ -22,10 +22,98 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        labelIPS = New Label()
+        tbIPS = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        tbIPK = New TextBox()
+        labelIPK = New Label()
+        labelPredikat = New Label()
+        SuspendLayout()
+        ' 
+        ' labelIPS
+        ' 
+        labelIPS.AutoSize = True
+        labelIPS.Location = New Point(144, 123)
+        labelIPS.Name = "labelIPS"
+        labelIPS.Size = New Size(86, 20)
+        labelIPS.TabIndex = 0
+        labelIPS.Text = "IP Semester"
+        ' 
+        ' tbIPS
+        ' 
+        tbIPS.Location = New Point(269, 120)
+        tbIPS.Name = "tbIPS"
+        tbIPS.Size = New Size(194, 27)
+        tbIPS.TabIndex = 1
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(269, 153)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 2
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(369, 153)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 3
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' tbIPK
+        ' 
+        tbIPK.Location = New Point(269, 206)
+        tbIPK.Name = "tbIPK"
+        tbIPK.ReadOnly = True
+        tbIPK.Size = New Size(194, 27)
+        tbIPK.TabIndex = 4
+        ' 
+        ' labelIPK
+        ' 
+        labelIPK.AutoSize = True
+        labelIPK.Location = New Point(144, 209)
+        labelIPK.Name = "labelIPK"
+        labelIPK.Size = New Size(106, 20)
+        labelIPK.TabIndex = 5
+        labelIPK.Text = "IPK Akumulatif"
+        ' 
+        ' labelPredikat
+        ' 
+        labelPredikat.AutoSize = True
+        labelPredikat.Location = New Point(269, 253)
+        labelPredikat.Name = "labelPredikat"
+        labelPredikat.Size = New Size(0, 20)
+        labelPredikat.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(labelPredikat)
+        Controls.Add(labelIPK)
+        Controls.Add(tbIPK)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(tbIPS)
+        Controls.Add(labelIPS)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents labelIPS As Label
+    Friend WithEvents tbIPS As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents tbIPK As TextBox
+    Friend WithEvents labelIPK As Label
+    Friend WithEvents labelPredikat As Label
 
 End Class
--- POSTTEST_1/Form1.vb
+++ POSTTEST_1/Form1.vb
@@ -1,3 +1,55 @@
 ﻿Public Class Form1
 
+    Dim totalIPS As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim ips As Double
+
+        'Validasi Input angka yang dikirim
+        If Double.TryParse(tbIPS.Text, ips) Then
+
+            If ips < 0 Or ips > 4 Then
+                MessageBox.Show("Masukkan angka IPS antara 0 dan 4!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                tbIPS.Clear()
+                tbIPS.Focus()
+                Exit Sub
+            End If
+
+            totalIPS += ips
+                jumlahSemester += 1
+
+                Dim ipk As Double = totalIPS / jumlahSemester
+                tbIPK.Text = ipk.ToString("0.00")
+
+                ' Menentukan Predikat
+                If ipk >= 2.0 And ipk <= 2.75 Then
+                    labelPredikat.Text = "Predikat: Cukup"
+                ElseIf ipk >= 2.76 And ipk <= 3.0 Then
+                    labelPredikat.Text = "Predikat: Memuaskan"
+                ElseIf ipk > 3.0 Then
+                    labelPredikat.Text = "Predikat: Sangat Memuaskan"
+                Else
+                    labelPredikat.Text = ""
+                End If
+
+                ' Kosongkan input
+                tbIPS.Clear()
+                tbIPS.Focus()
+
+            Else
+                MessageBox.Show("Masukkan angka IPS yang valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIPS = 0
+        jumlahSemester = 0
+        tbIPS.Clear()
+        tbIPK.Clear()
+        labelPredikat.Text = ""
+
+        tbIPS.Focus()
+    End Sub
+
 End Class
